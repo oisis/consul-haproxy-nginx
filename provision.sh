5d5548c2687ce70f0ba14db9a662b907af6049ef
OiSiS <oisis@o2.pl>
2016-11-20 22:20:00 +0100

    Back to localhost when setting K/V for HAProxy

--- provision.sh
+++ provision.sh
@@ -104,11 +104,11 @@ fi
 /usr/bin/consul agent -config-file /root/consul/consul-$HOSTNAME.json 2>&1 >/dev/null &
 
 if [[ `hostname` == "consul-server" ]]; then
-  /usr/bin/curl -s -X PUT -d '4096' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/maxconn
-  /usr/bin/curl -s -X PUT -d '5s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-connect
-  /usr/bin/curl -s -X PUT -d '50s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-client
-  /usr/bin/curl -s -X PUT -d '50s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-server
-  /usr/bin/curl -s -X PUT -d 'enable' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/stats
-  /usr/bin/curl -s -X PUT -d '5s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/refresh
-  /usr/bin/curl -s -X PUT -d '/' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/uri
+  /usr/bin/curl -s -X PUT -d '4096' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/maxconn
+  /usr/bin/curl -s -X PUT -d '5s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-connect
+  /usr/bin/curl -s -X PUT -d '50s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-client
+  /usr/bin/curl -s -X PUT -d '50s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-server
+  /usr/bin/curl -s -X PUT -d 'enable' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/stats
+  /usr/bin/curl -s -X PUT -d '5s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/refresh
+  /usr/bin/curl -s -X PUT -d '/' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/uri
 fi
