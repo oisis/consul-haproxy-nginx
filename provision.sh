0788ae371f096ad894d9adceb8fb3f579c122153
OiSiS <oisis@o2.pl>
2016-11-20 21:38:15 +0100

    Run K/V insert only on consul-server after consul daemon run

--- provision.sh
+++ provision.sh
@@ -82,13 +82,6 @@ if [[ `hostname` == "lb" ]]; then
   docker run -d --name haproxy -p 80:80 -p 1936:1936 -p 443:443 --restart unless-stopped \
     -v /root/haproxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
     -v /root/haproxy/haproxy.pem:/usr/local/etc/haproxy/cert.pem:ro haproxy:1.6.9-alpine
-  /usr/bin/curl -s -X PUT -d '4096' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/maxconn
-  /usr/bin/curl -s -X PUT -d '5s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-connect
-  /usr/bin/curl -s -X PUT -d '50s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-client
-  /usr/bin/curl -s -X PUT -d '50s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/timeout-server
-  /usr/bin/curl -s -X PUT -d 'enable' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/stats
-  /usr/bin/curl -s -X PUT -d '5s' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/refresh
-  /usr/bin/curl -s -X PUT -d '/' http://127.0.0.1:8500/v1/kv/prod/portal/haproxy/uri
 fi
 
 # Setup LoadBalancer Nginx
@@ -108,3 +101,13 @@ fi
 
 # Run consul
 /usr/bin/consul agent -config-file /root/consul/consul-$HOSTNAME.json 2>&1 >/dev/null &
+
+if [[ `hostname` == "consul-server" ]]; then
+  /usr/bin/curl -s -X PUT -d '4096' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/maxconn
+  /usr/bin/curl -s -X PUT -d '5s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-connect
+  /usr/bin/curl -s -X PUT -d '50s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-client
+  /usr/bin/curl -s -X PUT -d '50s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/timeout-server
+  /usr/bin/curl -s -X PUT -d 'enable' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/stats
+  /usr/bin/curl -s -X PUT -d '5s' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/refresh
+  /usr/bin/curl -s -X PUT -d '/' http://172.20.20.31:8500/v1/kv/prod/portal/haproxy/uri
+fi
